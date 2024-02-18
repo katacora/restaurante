4a4817be625c4bf2152f5bcab782c45fe8646190
katacora <claudine.delgado@gmail.com>
2024-02-18 20:41:34 +0100

    first commit

---
+++ app/Http/Controllers/ReservaController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Horario;
+use Illuminate\Http\Request;
+
+class ReservaController extends Controller
+{
+    function index(){
+        $horarios = Horario::all();
+        $eventos = [];
+        foreach ($horarios as $evento) {
+            if ($evento->disponible == 1) {
+                 $eventos[] = [
+                    'title'=> $evento->hora,
+                    'start'=> $evento->fecha,
+                    'url'=> "/clase/restaurante/public/confirmarReserva/".$evento->fecha."/".$evento->hora
+                ];
+            }    
+        }
+        return view('reservar',compact("eventos"));
+    }
+
+
+    function mostrarEventos(){
+        $horarios = Horario::all();
+        $eventos = [];
+        foreach ($horarios as $evento) {
+            if ($evento->disponible == 1) {
+                 $eventos[] = [
+                    'title'=> $evento->hora,
+                    'start'=> $evento->fecha,
+                    //'url'=> "/restauranteJeipri/public/confirmarReserva/".$evento->fecha."/".$evento->hora
+                ];
+            }
+        }
+        return response()->json($eventos);
+    
+    }
+}
